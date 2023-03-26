5bcfa710d449c3d2272d6ade29eb50e593498799
evidenciasponao <evidenciasponao@gmail.com>
2023-03-26 08:13:31 +0200

    cambio4

--- src/App.js
+++ src/App.js
@@ -25,7 +25,6 @@ function App() {
       <Contact/> */}
       <Routes>
         <Route path="/" element={<Home />}></Route>
-        <Route path="/myportfolio" element={<Home />}></Route>
         <Route path="/about" element={<About />}></Route>
         <Route path="/services" element={<Services />}></Route>
         <Route path="/contact" element={<Contact />}></Route>
