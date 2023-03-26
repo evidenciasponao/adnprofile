f83d93c8f372b8d2bfad4437300db6d95730c52e
evidenciasponao <evidenciasponao@gmail.com>
2023-03-26 08:22:00 +0200

    cambio3

--- src/Pages/PageNotFound.js
+++ src/Pages/PageNotFound.js
@@ -1,15 +1,89 @@
 import React from "react";
+import "./Home.css";
+import profileimg from "../assets/img/profileimg.png";
+import Pdf from "../assets/pdf/andersonmontenegro.pdf";
+import { Link } from "react-router-dom";
+import Linkedinicon from "../assets/img/socialicons/linkedin.png";
+import Githubicon from "../assets/img/socialicons/github.png";
+import MailIcon from "../assets/img/socialicons/email.png";
 
-import './PageNotFound.css';
-
-const PageNotFound = () => {
+const Home = () => {
   return (
-    <div id="pagenotfound" className="text-center" data-aos="fade-in">
+    <div id="home">
       <div className="container">
         <div className="row">
-          <div className="col-sm-12">
-            <h1>404</h1>
-            <h2>Page Not Found!</h2>
+          <div className="col-sm-5">
+            <div className="imgsec">
+              <img
+                src={profileimg}
+                data-aos="flip-right"
+                alt="Anderson Montenegro"
+                className="img-fluid"
+              />
+              <div className="socialmedia">
+                <a
+                  href="https://www.linkedin.com/in/anderson-montenegro-6498a3231/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img
+                    src={Linkedinicon}
+                    alt="linkedin"
+                    data-aos="fade-right"
+                    data-aos-delay="1000"
+                  />
+                </a>
+                <a
+                  href="https://github.com/evidenciasponao"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img
+                    src={Githubicon}
+                    alt="github"
+                    data-aos="fade-right"
+                    data-aos-delay="1000"
+                  />
+                </a>
+
+                <a
+                  href="mailto:evidenciasponao@gmail.com"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img
+                    src={MailIcon}
+                    alt="email"
+                    data-aos="fade-right"
+                    data-aos-delay="1000"
+                  />
+                </a>
+              </div>
+              <div className="bgblack" data-aos="fade-right"></div>
+            </div>
+          </div>
+          <div className="col-sm-5 offset-sm-2">
+            <div className="rightcontent" data-aos="fade-right">
+              <h1>Anderson Montenegro</h1>
+              <h2>Software Development</h2>
+              <br />
+              <div className="row">
+                <div className="col-sm-4 col-6">
+                  <a href={Pdf} target="_blank" rel="noreferrer">
+                    <button type="button" className="btn btn-primary">
+                      Resume
+                    </button>
+                  </a>
+                </div>
+                <div className="col-sm-5 col-6">
+                  <Link to="/contact">
+                    <button type="button" className="btn btn-primary">
+                      Hire Me
+                    </button>
+                  </Link>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -17,4 +91,4 @@ const PageNotFound = () => {
   );
 };
 
-export default PageNotFound;
+export default Home;
